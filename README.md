3eb32341ca0484ead0e1bfc57b76d07c78da855d
Mox Alehin <moxalehin@gmail.com>
2025-03-18 10:17:22 +0300

    Update person modal

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ alias: Family Tree
 Плагин для рендера семейных деревьев.
 Информация хранится в метаданных заметки человека
 Есть View в котором можно посмотреть дерево.
+
+![Person Modal](Images/FamilyTree_PersonModal.png)
--- src/modals/person-modal.ts
+++ src/modals/person-modal.ts
@@ -1,7 +1,7 @@
 import { App, Modal, Setting, TFile } from "obsidian";
 import { ImageSuggest, NoteSuggest } from "utils/suggest";
 
-enum FieldType {
+export enum FieldType {
     boolean,
     string,
     date,
@@ -9,7 +9,7 @@ enum FieldType {
     image
 }
 
-enum Field {
+export enum Field {
     name = 'Name',
     middleName = 'Middle Name',
     surname = 'Surname',
@@ -20,7 +20,7 @@ enum Field {
     father = 'Father',
 }
 
-const PersonFieldTypes: Record<Field, FieldType> = {
+export const PersonFieldTypes: Record<Field, FieldType> = {
     [Field.name]: FieldType.string,
     [Field.middleName]: FieldType.string,
     [Field.surname]: FieldType.string,
@@ -31,7 +31,7 @@ const PersonFieldTypes: Record<Field, FieldType> = {
     [Field.father]: FieldType.file,
 };
 
-interface PersonFields extends Record<Field, any> { }
+export interface PersonFields extends Record<Field, any> { }
 
 function getPersonFromFile(app: App, file: TFile): PersonFields {
     const person = {} as PersonFields;
@@ -42,13 +42,7 @@ function getPersonFromFile(app: App, file: TFile): PersonFields {
 
     Object.values(Field).forEach((key) => {
         if (key in metadata) {
-            const value = metadata[key];
-
-            if ((key === Field.birth || key === Field.death) && typeof value === 'string') {
-                person[key] = new Date(value);
-            } else {
-                person[key] = value;
-            }
+            person[key] = metadata[key];
         }
     });
     return person;
@@ -57,20 +51,17 @@ function getPersonFromFile(app: App, file: TFile): PersonFields {
 export class PersonModal extends Modal {
     person: PersonFields;
     file: TFile;
+
     constructor(app: App, file: TFile) {
         super(app);
-        this.person = getPersonFromFile(app, file)
+        this.file = file;
+        this.person = getPersonFromFile(app, file);
     }
 
     onOpen() {
         const { contentEl } = this;
         contentEl.empty();
         this.setTitle("Edit Person");
-
-        const activeFile = this.app.workspace.getActiveFile();
-        if (activeFile) {
-            getPersonFromFile(this.app, activeFile);
-        }
 
         Object.entries(PersonFieldTypes).forEach(([field, fieldType]) => {
             this.addField(contentEl, field as Field, fieldType);
@@ -79,74 +70,84 @@ export class PersonModal extends Modal {
         new Setting(contentEl)
             .addButton((btn) =>
                 btn.setIcon("check").setCta().onClick(() => {
-                    onSubmitHandler();
+                    this.handleSubmit();
                 })
             );
+
         this.scope.register([], "Enter", (evt) => {
             evt.preventDefault();
-            onSubmitHandler();
+            this.handleSubmit();
         });
+    }
 
-        const onSubmitHandler = () => {
-            this.close();
-        };
+    private handleSubmit() {
+        this.app.fileManager.processFrontMatter(this.file, (frontmatter) => {
+            Object.values(Field).forEach((field) => {
+                delete frontmatter[field];
+            });
+            Object.values(Field).forEach((field) => {
+                if (field in this.person) {
+                    frontmatter[field] = this.person[field];
+                }
+            });
+        })
+        this.close();
     }
 
     private addField(contentEl: HTMLElement, field: Field, type: FieldType) {
         const setting = new Setting(contentEl).setName(field);
-    
-        // Берём текущее значение из this.person или устанавливаем дефолт
+
         const currentValue = this.person[field] ?? this.getDefaultValue(type);
-    
-        const handleChange = (value: any) => {
+
+        const handleChange = (value: any, valueToWrite: any = value) => {
             if (value === "" || value === null || value === undefined) {
-                delete this.person[field]; // Удаляем поле, если значение пустое
+                delete this.person[field];
             } else {
-                this.person[field] = value; // Иначе сохраняем значение
+                this.person[field] = valueToWrite;
             }
         };
-    
+
         switch (type) {
             case FieldType.boolean:
                 setting.addToggle((toggle) =>
                     toggle
                         .setValue(Boolean(currentValue))
-                        .onChange((value) => handleChange(value))
+                        .onChange(handleChange)
                 );
                 break;
-    
             case FieldType.date:
                 setting.addText((text) => {
                     text
-                        .setValue(currentValue instanceof Date ? this.formatDate(currentValue) : "")
-                        .onChange((value) => handleChange(value ? new Date(value) : null));
-                    text.inputEl.type = "date";
+                        // .setValue(currentValue instanceof Date ? this.formatDate(currentValue) : "")
+                        .setValue(currentValue as string ?? "")
+                        .setPlaceholder('YYYY-MM-DD')
+                        .onChange(handleChange);
+                    // text.inputEl.type = "date";
                 });
                 break;
-    
             case FieldType.string:
                 setting.addText((text) =>
                     text
                         .setValue(currentValue as string ?? "")
-                        .onChange((value) => handleChange(value))
+                        .onChange(handleChange)
                 );
                 break;
-    
             case FieldType.file:
                 setting.addText((text) => {
                     new NoteSuggest(this.app, text.inputEl);
                     text
                         .setValue((currentValue as string)?.replace(/^\[\[/, '').replace(/\]\]$/, '') ?? "")
-                        .onChange((value) => handleChange(value));
+                        .onChange(value => {
+                            handleChange(value, `[[${value}]]`)
+                        });
                 });
                 break;
-    
             case FieldType.image:
                 setting.addText((text) => {
                     new ImageSuggest(this.app, text.inputEl);
                     text
                         .setValue(currentValue as string ?? "")
-                        .onChange((value) => handleChange(value));
+                        .onChange(handleChange);
                 });
                 break;
         }
@@ -160,9 +161,5 @@ export class PersonModal extends Modal {
             case FieldType.file: return "";
             case FieldType.image: return "";
         }
-    }
-
-    private formatDate(date: Date): string {
-        return date.toISOString().split("T")[0]; // YYYY-MM-DD
     }
 }
